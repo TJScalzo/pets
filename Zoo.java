76d29ae76994491fe7501e6bedf1024098cdb7ee
TJScalzo <timtv25@gmail.com>
2017-02-07 11:09:27 -0500

    Challenge 3
    Modified constructors to use loop & any number of pets

--- Zoo.java
+++ Zoo.java
@@ -6,21 +6,37 @@ public class Zoo
 {
     //fields
     private Pet[] theZoo;
+    private int numPets;
     
     //constructors
     public Zoo()
     {
-        theZoo = new Pet[10];
-        theZoo[0] = new Pet("Gerald");
-        theZoo[1] = new Macaw();
-        theZoo[2] = new Pet("Giblet");
-        theZoo[3] = new Macaw("Sandy");
-        theZoo[4] = new Pet("Hector");
+        numPets = 10;
+        theZoo = createZoo(numPets);
     }
-
+    
+    public Zoo(int numPets)
+    {
+        this.numPets = numPets;
+        theZoo = createZoo(numPets);
+    }
+    
+    //methods
+    private Pet[] createZoo(int numPets)
+    {
+        Pet[] zoo = new Pet[numPets];
+        for(int i = 0; i < numPets; i++) {
+            if(Math.random() < 0.5)
+                zoo[i] = new Pet("Gerald");
+            else
+                zoo[i] = new Macaw();
+        }
+        return zoo;
+    }
+    
     public void soundOff()
     {
-        for(int i = 0; i < theZoo.length; i++) {
+        for(int i = 0; i < numPets; i++) {
             System.out.print("Area " + (i+1) + ": ");
             if(theZoo[i] != null)
                 System.out.println("\"" + theZoo[i].speak() + "\" says " + theZoo[i].getName() + ".");
