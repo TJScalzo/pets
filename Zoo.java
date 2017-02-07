efc8074eba926bf7cc5d621874c6c4699afd53e1
TJScalzo <timtv25@gmail.com>
2017-02-07 10:55:59 -0500

    Challenge 2
    Recreated Zoo class to match instructions

--- Zoo.java
+++ Zoo.java
@@ -1,8 +1,6 @@
-
 /**
  * This class represent a collection of Pet objects
  * including objects of any class which IS-A Pet
- * @author (Tim Barron) 
  */
 public class Zoo
 {
@@ -12,18 +10,22 @@ public class Zoo
     //constructors
     public Zoo()
     {
-        theZoo = new Pet[40];
-        theZoo[0] = new Pet("boring pet");
+        theZoo = new Pet[10];
+        theZoo[0] = new Pet("Gerald");
+        theZoo[1] = new Macaw();
+        theZoo[2] = new Pet("Giblet");
+        theZoo[3] = new Macaw("Sandy");
+        theZoo[4] = new Pet("Hector");
     }
 
     public void soundOff()
     {
-        for(int i = 0; i < theZoo.length; i++)
-        {
+        for(int i = 0; i < theZoo.length; i++) {
+            System.out.print("Area " + (i+1) + ": ");
             if(theZoo[i] != null)
-                System.out.println("The " + theZoo[i].getName() + " says " + theZoo[i].getName());
+                System.out.println("\"" + theZoo[i].speak() + "\" says " + theZoo[i].getName() + ".");
             else
-                System.out.println("There is no animal in that spot in the Zoo.");
+                System.out.println("There is no animal in this area of the Zoo.");
         }
     }
 }
